5a33e466a097978d3959c3b8a1dd6d1cc506166b
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2025-10-31 23:14:21 +0300

    add: implemented operation of the "Create" and "Delete" buttons

---
+++ Tetris-Inventory.Unity/Assets/_Project/Develop/Alexey Pasechnik/Scripts/InventoryHandler.cs
@@ -0,0 +1,48 @@
+using System;
+using _Project.Services;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace _Project.Develop.Alexey_Pasechnik.Scripts
+{
+    public class InventoryHandler : MonoBehaviour, IInventoryHandler
+    {
+        public event Action OnRequestCreateItem;
+        public event Action OnRequestTakeItem;
+        public event Action OnRequestDeleteItem;
+        public event Action OnRequestPlaceItem;
+
+        private UIDocument _document;
+        private VisualElement _root;
+
+        //HACK: Затычка
+        private void Awake()
+        {
+            Initialize(GetComponent<UIDocument>());
+        }
+
+        public void Initialize(UIDocument document)
+        {
+            _document = document;
+
+            if (_document == null) return;
+
+            _root = _document.rootVisualElement;
+
+            InitializeCreateButton();
+            InitializeDeleteButton();
+        }
+
+        private void InitializeCreateButton()
+        {
+            var createButton = _root.Q<Button>("CreateButton");
+            createButton.RegisterCallback<ClickEvent>(evt => OnRequestCreateItem?.Invoke());
+        }
+
+        private void InitializeDeleteButton()
+        {
+            var deleteButton = _root.Q<Button>("DeleteButton");
+            deleteButton.RegisterCallback<PointerOverEvent>(evt => OnRequestDeleteItem?.Invoke());
+        }
+    }
+}
