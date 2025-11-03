e72791501668df36d31d6617d4fd9cb29cea297d
Maxim S. <its3zin.main@gmail.com>
2025-11-03 22:20:51 +0300

    add: inventory presenter

---
+++ Tetris-Inventory.Unity/Assets/Runtime/InventorySystem/Presenter/InventoryPresenter.cs
@@ -0,0 +1,50 @@
+using Runtime.InventorySystem.Model;
+using UnityEngine;
+
+namespace Runtime.InventorySystem.Presenter
+{
+    public class InventoryPresenter
+    {
+        private IInventory _preview;
+        
+        private IInventory _inventory;
+
+        private Item _cachedItem;
+
+        private IInventory _cachedInventory;
+        
+        private Vector2 _cachedPosition;
+        
+        public InventoryPresenter(IInventory preview, IInventory inventory)
+        {
+            _preview = preview;
+            
+            _inventory = inventory;
+        }
+
+        private void OnTakeItem(IInventory inventory, Vector2Int position)
+        {
+            var item = inventory.GetItem(position);
+            if (item == null) return;
+            
+            _cachedItem = item;
+            _cachedInventory = inventory;
+            _cachedPosition = position;
+        }
+
+        private void OnPlaceItem(IInventory inventory, Vector2Int position)
+        {
+            var success = inventory.CanPlaceItem(_cachedItem, position);
+
+            if (!success) _cachedInventory.TryPlaceItem(_cachedItem, position);
+            else ClearCache();
+        }
+
+        private void ClearCache()
+        {
+            _cachedItem = null;
+            _cachedInventory = null;
+            _cachedPosition = default;
+        }
+    }
+}
