5f4168fda3aa2b0eb85ca307f8fccb65152c860c
Valyutik <sergei.lind@tutamail.com>
2025-11-04 20:42:42 +0300

    add: custom inspector for item config

---
+++ Tetris-Inventory.Unity/Assets/Editor/ItemConfigEditor.cs
@@ -0,0 +1,61 @@
+using Runtime.InventorySystem.Config;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(ItemConfig))]
+    public sealed class ItemConfigEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var config = (ItemConfig)target;
+            config.id = EditorGUILayout.TextField("ID", config.id);
+            config.displayName = EditorGUILayout.TextField("Display Name", config.displayName);
+            
+            EditorGUILayout.LabelField("Description", EditorStyles.boldLabel);
+            config.description = EditorGUILayout.TextArea(config.description, GUILayout.MinHeight(60));
+            
+            config.icon = (Sprite)EditorGUILayout.ObjectField("Icon", config.icon, typeof(Sprite), false);
+            
+            EditorGUILayout.Space(10); 
+            EditorGUILayout.LabelField("Shape", EditorStyles.boldLabel);
+            
+            config.width = Mathf.Max(1, EditorGUILayout.IntField("Width", config.width));
+            config.height = Mathf.Max(1, EditorGUILayout.IntField("Height", config.height));
+            
+            var totalCells = config.width * config.height;
+            
+            while (config.flatShape.Count < totalCells) config.flatShape.Add(false);
+
+            while (config.flatShape.Count > totalCells) config.flatShape.RemoveAt(config.flatShape.Count - 1);
+
+            for (var y = 0; y < config.height; y++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                for (var x = 0; x < config.width; x++)
+                {
+                    var index = y * config.width + x;
+                    config.flatShape[index] = GUILayout.Toggle(config.flatShape[index], GUIContent.none, GUILayout.Width(20));
+                    
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            
+            EditorGUILayout.Space(5);
+
+            if (GUILayout.Button("Clear Shape"))
+            {
+                for (var i = 0; i < config.flatShape.Count; i++)
+                {
+                    config.flatShape[i] = false;
+                }
+            }
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(config);
+            }
+        }
+    }
+}
