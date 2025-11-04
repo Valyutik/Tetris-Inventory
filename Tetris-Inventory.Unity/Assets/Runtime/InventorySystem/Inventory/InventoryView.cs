ddaee1e6384ebde2a0442b384544020af72ab349
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2025-11-04 23:06:13 +0300

    chg: refactor

---
+++ Tetris-Inventory.Unity/Assets/Runtime/InventorySystem/Inventory/InventoryView.cs
@@ -0,0 +1,41 @@
+using UnityEngine.UIElements;
+
+namespace Runtime.InventorySystem.Inventory
+{
+    public class InventoryView
+    {
+        private const int CellSize = 100;
+        
+        private int _gridWidth;
+        private int _gridHeight;
+
+        private readonly VisualElement _inventoryGrid;
+        
+        public InventoryView(int gridWidth, int gridHeight, UIDocument document)
+        {
+            _gridWidth = gridWidth;
+            _gridHeight = gridHeight;
+
+            var root = document.rootVisualElement;
+            
+            var inventory =root.Q<VisualElement>("Inventory");
+            _inventoryGrid = inventory.Q<VisualElement>("Grid");
+        }
+        
+        public void SetUpGrid(int width, int height)
+        {
+            _inventoryGrid.style.height = height * CellSize;
+            _inventoryGrid.style.width = width * CellSize;
+        }
+
+        public VisualElement CreateCell()
+        {
+            var cell = new VisualElement();
+            cell.AddToClassList("cell");
+            
+            _inventoryGrid.Add(cell);
+
+            return cell;
+        }
+    }
+}
