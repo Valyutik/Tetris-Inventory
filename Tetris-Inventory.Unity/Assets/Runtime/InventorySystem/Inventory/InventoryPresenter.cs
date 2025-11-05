0ebeb68b1c9f971cbb81806f75fd3d36784069dd
Maxim S. <its3zin.main@gmail.com>
2025-11-05 16:57:07 +0300

    chg: connects presenter to view and model

--- Tetris-Inventory.Unity/Assets/Runtime/InventorySystem/Inventory/InventoryPresenter.cs
+++ Tetris-Inventory.Unity/Assets/Runtime/InventorySystem/Inventory/InventoryPresenter.cs
@@ -1,50 +1,109 @@
 using Runtime.InventorySystem.Common;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Runtime.InventorySystem.Inventory
 {
     public class InventoryPresenter
     {
-        private IInventory _preview;
+        private readonly InventoryModel _model;
         
-        private IInventory _inventory;
+        private readonly InventoryView _view;
 
         private Item _cachedItem;
+        
+        private Vector2Int _cachedPosition;
 
-        private IInventory _cachedInventory;
+        private VisualElement[,] _cells;
         
-        private Vector2 _cachedPosition;
-        
-        public InventoryPresenter(IInventory preview, IInventory inventory)
+        public InventoryPresenter(InventoryView view, InventoryModel model)
         {
-            _preview = preview;
+            _view = view;
             
-            _inventory = inventory;
+            _model = model;
+            
+            Init();
         }
 
-        private void OnTakeItem(IInventory inventory, Vector2Int position)
+        private void Init()
         {
-            var item = inventory.GetItem(position);
-            if (item == null) return;
+            _view.SetUpGrid(_model.Width, _model.Height);
+            
+            _cells = new VisualElement[_model.Width, _model.Height];
+            
+            for (var y = 0; y < _model.Height; y++)
+            {
+                for (var x = 0; x < _model.Width; x++)
+                {
+                    var visualElement = _view.CreateCell();
+                    
+                    var targetPosition = new Vector2Int(x, y);
+
+                    visualElement.RegisterCallback<PointerDownEvent>(_ => OnTakeItem(targetPosition));
+                    
+                    visualElement.RegisterCallback<PointerUpEvent>(_ => OnPlaceItem(targetPosition));
+                    
+                    _cells[x, y] = visualElement;
+                }
+            }
+            
+            UpdateView();
+        }
+
+        private void OnTakeItem(Vector2Int position)
+        {
+            //TODO: Fix the _model.
+            var item =  _model.GetItem(position);
+            Debug.Log($"Item: {item}");
+            Debug.Log($"CachedItem: {_cachedItem}");
+            
+            if (item == null || _cachedItem != null) return;
+            
+            Debug.Log($"[InventoryPresenter] OnTakeItem: {position}]");
             
             _cachedItem = item;
-            _cachedInventory = inventory;
+            
             _cachedPosition = position;
+            
+            _model.TryRemoveItem(position);
+            
+            UpdateView();
         }
 
-        private void OnPlaceItem(IInventory inventory, Vector2Int position)
+        private void OnPlaceItem(Vector2Int position)
         {
-            var success = inventory.CanPlaceItem(_cachedItem, position);
+            Debug.Log($"OnClickUp");
+            
+            if (_cachedItem == null) return;
+            
+            Debug.Log($"[InventoryPresenter] OnPlaceItem: {position}]");
+            
+            var success = _model.TryPlaceItem(_cachedItem, position);
 
-            if (!success) _cachedInventory.TryPlaceItem(_cachedItem, position);
-            else ClearCache();
-        }
-
-        private void ClearCache()
-        {
+            if (!success) _model.TryPlaceItem(_cachedItem, _cachedPosition);
+            
             _cachedItem = null;
-            _cachedInventory = null;
-            _cachedPosition = default;
+            
+            UpdateView();
+        }
+
+        // TODO: Move this method to View
+        private void UpdateView()
+        {
+            for (var y = 0; y < _model.Width; y++)
+            {
+                for (var x = 0; x < _model.Height; x++)
+                {
+                    var targetPosition = new Vector2Int(x, y);
+                    
+                    var visualElement = _cells[x, y];
+
+                    var color = _model.IsCellOccupied(targetPosition) ? Color.gray : Color.red; 
+                    
+                    visualElement.style.backgroundColor = color;
+                }
+            }
         }
     }
 }
