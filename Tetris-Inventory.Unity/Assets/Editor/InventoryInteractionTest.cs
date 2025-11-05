bb65a3051191bdd8a9a83e2b6e4c996e042fd78a
Maxim S. <its3zin.main@gmail.com>
2025-11-05 16:57:36 +0300

    add: test scene inventory interaction

---
+++ Tetris-Inventory.Unity/Assets/Editor/InventoryInteractionTest.cs
@@ -0,0 +1,37 @@
+using Runtime.InventorySystem.Common;
+using Runtime.InventorySystem.Inventory;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Runtime.Core
+{
+    public class InventoryInteractionTest : MonoBehaviour
+    {
+        [SerializeField] private Vector2Int _inventorySize;
+        
+        [SerializeField] private UIDocument _document;
+        
+        private InventoryModel _model;
+        
+        private InventoryView _view;
+        
+        private InventoryPresenter _presenter;
+        
+        private void Awake()
+        {
+            _model = new InventoryModel(_inventorySize.x, _inventorySize.y);
+            
+            var item = new Item("0", "Test", "Description", new bool[,] 
+                {   { true, true }, 
+                    { true, false } 
+                });
+            
+            _model.TryPlaceItem(item, new Vector2Int(2, 0));
+
+            _view = new InventoryView(_inventorySize.x, _inventorySize.y, _document);
+
+            _presenter = new InventoryPresenter(_view, _model);
+            
+        }
+    }
+}
