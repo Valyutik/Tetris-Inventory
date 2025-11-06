13196cd66c1bf945f48f7a6719818c647982b3ba
Valyutik <sergei.lind@tutamail.com>
2025-11-06 22:43:56 +0300

    add: connection between item generator and stash

--- Tetris-Inventory.Unity/Assets/Runtime/Core/EntryPoint.cs
+++ Tetris-Inventory.Unity/Assets/Runtime/Core/EntryPoint.cs
@@ -1,51 +1,62 @@
-using System.Collections.Generic;
-using System.Linq;
-using Runtime.InventorySystem.Common;
-using Runtime.InventorySystem.DeleteArea;
 using Runtime.InventorySystem.DeleteConfirmation;
+using Runtime.InventorySystem.ItemGeneration;
+using Runtime.InventorySystem.DeleteArea;
 using Runtime.InventorySystem.Inventory;
-using UnityEngine;
+using Runtime.InventorySystem.Common;
+using Runtime.InventorySystem.Stash;
+using System.Collections.Generic;
 using UnityEngine.UIElements;
+using System.Linq;
+using UnityEngine;
 
 namespace Runtime.Core
 {
     public class EntryPoint : MonoBehaviour
     {
-        [SerializeField] private UIDocument _mainUIDocument;
+        [SerializeField] private UIDocument _document;
         [SerializeField] private Vector2Int _inventorySize;
         [SerializeField] private List<ItemConfig> _itemConfigs;
         
         [Header("Popup")]
         [SerializeField] private VisualTreeAsset _deleteConfirmationAsset;
         [SerializeField] private UIDocument _popupUIDocument;
-
-        private readonly List<Item> _itemsList = new();
-
+        
+        private StashPresenter _stashPresenter;
+    
         private void Start()
         {
-            var inventoryView = new InventoryView(_mainUIDocument);
+            var itemDatabase = new ItemDatabase(ItemConfigLoader.LoadAll());
+            
+            
+            var inventoryView = new InventoryView(_document);
 
             var inventoryModel = new InventoryModel(_inventorySize.x, _inventorySize.y);
-            
-            foreach (var item in _itemConfigs.Select(itemConfig => new Item(itemConfig.id, itemConfig.name, 
+
+            foreach (var item in _itemConfigs.Select(itemConfig => new Item(itemConfig.id, itemConfig.name,
                          itemConfig.description, itemConfig.color,
                          itemConfig.GetShapeMatrix())))
             {
-                _itemsList.Add(item);
+                inventoryModel.TryPlaceItem(item, new Vector2Int(2, 0));
             }
-            
-            //TODO: Testing place item in grid
-            inventoryModel.TryPlaceItem(_itemsList[0], new Vector2Int(0, 0));
-            inventoryModel.TryPlaceItem(_itemsList[1], new Vector2Int(2, 0));
-            
-            var inventoryPresenter = new InventoryPresenter(inventoryView, inventoryModel);
 
-            var deleteArea = new DeleteAreaView(_mainUIDocument.rootVisualElement.Q<Button>("DeleteButton"));
+            var inventoryPresenter = new InventoryPresenter(inventoryView, inventoryModel);
+            
+            var stashView = new StashView(_document.rootVisualElement);
+            var stashModel = new StashModel();
+            _stashPresenter = new StashPresenter(stashView, stashModel);
+            _stashPresenter.Initialize();
+
+            var itemGenerationModel = new ItemGenerationModel(itemDatabase.GetAllItems().ToList());
+            var itemGenerationView = new ItemGenerationView(_document.rootVisualElement);
+            var itemGenerationPresenter = new ItemGenerationPresenter(itemGenerationView, itemGenerationModel);
+            itemGenerationPresenter.OnItemGenerated += _stashPresenter.ShowItem;
+            
+            var deleteArea = new DeleteAreaView(_document.rootVisualElement.Q<Button>("DeleteButton"));
 
             var deleteConfirmation = new DeleteConfirmationView(_popupUIDocument, _deleteConfirmationAsset);
             
-            var gameLoop = new GameLoop(inventoryPresenter, deleteArea, deleteConfirmation);
-            
+            var gameLoop = new GameLoop(inventoryPresenter, _stashPresenter, deleteArea, deleteConfirmation);
+
             gameLoop.Run();
         }
     }
