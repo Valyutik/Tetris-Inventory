6d1a441dfaaf3683f3ec9a83d03b221b634a4095
Valyutik <sergei.lind@tutamail.com>
2025-11-07 14:00:45 +0300

    refactor: move the initialization of each module to its own method

--- Tetris-Inventory.Unity/Assets/Runtime/Core/EntryPoint.cs
+++ Tetris-Inventory.Unity/Assets/Runtime/Core/EntryPoint.cs
@@ -6,7 +6,6 @@ using Runtime.InventorySystem.DeleteArea;
 using Runtime.InventorySystem.Inventory;
 using Runtime.InventorySystem.Common;
 using Runtime.InventorySystem.Stash;
-using System.Collections.Generic;
 using UnityEngine.UIElements;
 using System.Linq;
 using UnityEngine;
@@ -17,7 +16,6 @@ namespace Runtime.Core
     {
         [SerializeField] private UIDocument _document;
         [SerializeField] private Vector2Int _inventorySize;
-        [SerializeField] private List<ItemConfig> _itemConfigs;
         
         [Header("UI Elements")]
         [SerializeField] private VisualTreeAsset _inventory;
@@ -29,48 +27,65 @@ namespace Runtime.Core
         [SerializeField] private VisualTreeAsset _deleteConfirmationAsset;
         [SerializeField] private UIDocument _popupUIDocument;
         
+        private InventoryPresenter _inventoryPresenter;
         private StashPresenter _stashPresenter;
+        private DragDropHandler _dragDropHandler;
+        private ItemGenerationPresenter _itemGenerationPresenter;
     
         private void Start()
         {
+            InitializeUI();
+            InitializeInventory();
+            InitializeStash();
+            InitializeItemGeneration();
+            InitializeDeleteSystem();
+            InitializeDragAndDrop();
+        }
+        
+        private void InitializeUI()
+        {
             var contentView = new ContentView(_document);
-            
             contentView.AddElement(_stash);
             contentView.AddElement(_createItemButton);
             contentView.AddElement(_deleteItemButton);
             contentView.AddElement(_inventory);
-            
-            var itemDatabase = new ItemDatabase(ItemConfigLoader.LoadAll());
-            
+        }
+
+        private void InitializeInventory()
+        {
             var inventoryView = new InventoryView(_document.rootVisualElement.Q<VisualElement>("InventoryGrid"));
-
             var inventoryModel = new InventoryModel(_inventorySize.x, _inventorySize.y);
+            _inventoryPresenter = new InventoryPresenter(inventoryView, inventoryModel);
+        }
 
-            foreach (var item in _itemConfigs.Select(itemConfig => new Item(itemConfig.id, itemConfig.name,
-                         itemConfig.description, itemConfig.color,
-                         itemConfig.GetShapeMatrix())))
-            {
-                inventoryModel.TryPlaceItem(item, new Vector2Int(2, 0));
-            }
-
-            var inventoryPresenter = new InventoryPresenter(inventoryView, inventoryModel);
-            
+        private void InitializeStash()
+        {
             var stashView = new InventoryView(_document.rootVisualElement.Q<VisualElement>("StashGrid"));
             var stashModel = new StashModel();
             _stashPresenter = new StashPresenter(stashView, stashModel);
+        }
 
+        private void InitializeItemGeneration()
+        {
+            var itemDatabase = new ItemDatabase(ItemConfigLoader.LoadAll());
             var itemGenerationModel = new ItemGenerationModel(itemDatabase.GetAllItems().ToList());
             var itemGenerationView = new ItemGenerationView(_document.rootVisualElement);
-            var itemGenerationPresenter = new ItemGenerationPresenter(itemGenerationView, itemGenerationModel);
-            itemGenerationPresenter.OnItemGenerated += _stashPresenter.SetItem;
-            
+
+            _itemGenerationPresenter = new ItemGenerationPresenter(itemGenerationView, itemGenerationModel);
+            _itemGenerationPresenter.OnItemGenerated += _stashPresenter.SetItem;
+        }
+
+        private void InitializeDeleteSystem()
+        {
             var deleteArea = new DeleteAreaView(_document.rootVisualElement.Q<Button>("DeleteButton"));
-
             var deleteConfirmation = new DeleteConfirmationView(_popupUIDocument, _deleteConfirmationAsset);
-            
-            var gameLoop = new DragDropHandler(inventoryPresenter, _stashPresenter, deleteArea, deleteConfirmation);
 
-            gameLoop.Init(_document.rootVisualElement);
+            _dragDropHandler = new DragDropHandler(_inventoryPresenter, _stashPresenter, deleteArea, deleteConfirmation);
+        }
+
+        private void InitializeDragAndDrop()
+        {
+            _dragDropHandler.Init(_document.rootVisualElement);
         }
     }
 }
